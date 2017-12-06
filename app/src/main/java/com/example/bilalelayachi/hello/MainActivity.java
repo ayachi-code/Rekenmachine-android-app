470c3296eb25ac9910c30c187f75da9f08b428e7
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-06 13:37:52 +0100

    Extra knop toegevoegt

--- app/src/main/java/com/example/bilalelayachi/hello/MainActivity.java
+++ app/src/main/java/com/example/bilalelayachi/hello/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.bilalelayachi.hello;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -30,20 +24,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-
 
     }
 
+
+
+    public void extra(View v) {
+            Button naar_extra_knop = (Button) findViewById(R.id.naar_extra);
+
+            Intent intent = new intent();
+
+
+
+            System.out.println("hey");
+    }
 
 
     public void plus(View v) {
@@ -65,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void klik (View v) {
+    public void klik(View v) {
+
 
 
 
@@ -106,25 +101,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
+
 }
